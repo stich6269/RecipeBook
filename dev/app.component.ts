8bcb03a89bfd91bd4b202ade66a73021a6bbd6e3
Artem Polishchuk (Kharkiv) <a.polishchuk@mobidev.biz>
2016-07-01 11:28:22 +0300

    - types of binding

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,16 +1,34 @@
 import {Component} from 'angular2/core';
-import {PuzzleComponent} from './puzzle/puzzle.component'
-
 
 @Component({
     selector: 'app',
     template: `
-        <my-puzzle></my-puzzle>
+        <!--string interpolation-->
+        <input type="text" value="{{'Artem'}}">
+        <br><br><br>
+        
+        <!--property binding-->
+        <input type="text" [value]="name" [ngClass]="{red: true}">
+        <br><br><br>
+        
+        <!--event binding-->
+        <input type="text" (keyup)="onKey(nameInp.value)" #nameInp>
+        <p>{{resultStr}}</p>
+        <br><br><br>
+        
+        <!--two ways binding-->
+        <input type="text" [(ngModel)]="name">
+        <p>Your name: {{name}}</p>
     `,
-    directives: [PuzzleComponent]
+    directives: []
 })
 
 
-export class AppComponent {
+export class AppComponent{
+    name: string = 'Artem';
+    resultStr: string = '';
 
+    onKey(keyValue: string): void{
+        this.resultStr += keyValue + ' | ';
+    }
 }
