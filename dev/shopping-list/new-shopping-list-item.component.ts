02a499bfca826f6ac8fa79d6e7d21993ef862c84
Artem Polishchuk (Kharkiv) <a.polishchuk@mobidev.biz>
2016-07-05 16:26:18 +0300

    - add form processing to  the app

--- dev/shopping-list/new-shopping-list-item.component.ts
+++ dev/shopping-list/new-shopping-list-item.component.ts
@@ -1,32 +1,51 @@
-import {Component} from "angular2/core";
+import {Component, OnInit} from "angular2/core";
 import {DataService} from "./services/data.services";
+import {ControlGroup, FormBuilder, Validators, Control} from "angular2/common";
 
 @Component({
     selector: 'shopping-list-new-item',
     template: `
-        <div class="input">
-            <label for="item-name">Name</label>
-            <input type="text" id="item-name" [(ngModel)]="item.name">
-        </div>
+        <form [ngFormModel]="myForm" (ngSubmit)="onSubmit()">
+            <div class="input">
+                <label for="item-name">Name</label>
+                <input type="text" id="item-name" [(ngModel)]="item.name" [ngFormControl]="myForm.controls['itemName']">
+            </div>
+            
+            <div class="input">
+                <label for="item-amount">Amount</label>
+                <input type="text" id="item-amount" [(ngModel)]="item.amount"  [ngFormControl]="myForm.controls['amount']">
+            </div>
+            
+            <button type="submit" [disabled]="!myForm.valid">Add Item</button>
+        </form>
         
-        <div class="input">
-            <label for="item-amount">Amount</label>
-            <input type="text" id="item-amount" [(ngModel)]="item.amount">
-        </div>
-        
-        <button (click)="onAddItem()">Add Item</button>
     `
 })
 
-export class NewShoppingListComponent{
+export class NewShoppingListComponent implements OnInit{
     item = {name: '', amount: 0};
-    constructor(private _dataService: DataService){};
-    onAddItem(): void{
-        if(this.item.name && this.item.amount){
-            this._dataService.insertItem(this.item);
-            this.item = {name: '', amount: 0};
-        }else{
-            alert('Complete the fields and try again')
-        }
+    myForm: ControlGroup;
+    constructor(private _dataService: DataService, private _formBuilder: FormBuilder){};
+    onSubmit(){
+        this._dataService.insertItem(this.item);
+        console.log(this.item);
+        this.item = {name: '', amount: 0};
+    }
+
+    ngOnInit(): void{
+        this.myForm = this._formBuilder.group({
+            itemName: ['', Validators.required],
+            amount: ['', Validators.compose([
+                Validators.required,
+                amountOver
+            ])]
+        });
+    }
+}
+
+
+function amountOver(item: Control): {[s: string]: boolean}{
+    if(item.value <=0){
+        return {notEnough: true}
     }
 }
